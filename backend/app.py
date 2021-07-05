2dea3a4cdeb52b8792ae557be453798afabe2c72
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-07-05 17:55:55 -0500

    Password encryption with bcrypt

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
-CORS(app)
 app.config['ENV'] = "development"
 
+# Set up some configurations for the app
+CORS(app)
+bcrypt = Bcrypt(app)
+
+# Root endpoint
 @app.route("/")
 def index():
     return "Hello World!"
 
+# Insert user
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    pass
+
+# Main method
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
--- database/trading.sql
+++ database/trading.sql
@@ -1,5 +1,7 @@
+-- For a single user
 CREATE TABLE IF NOT EXISTS trader {
     ID INT NOT NULL,
-    Name VARCHAR(20) UNIQUE NOT NULL,
-    Password VARCHAR(30) NOT NULL
+    Full_Name VARCHAR(50),
+    Username VARCHAR(30) UNIQUE NOT NULL,
+    User_Password VARCHAR(30) NOT NULL
 }
