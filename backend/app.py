f1e9314e3db00e633760bee17051eb5ab2fdb10c
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-07-18 19:54:05 -0500

    Flask ext fix

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from flask.ext.bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['ENV'] = "development"
