7d936140be4ef02c7cf3226f7dfaf31a4f7b4987
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-07-24 22:37:45 -0500

    Database change

--- database/trading.sql
+++ database/trading.sql
@@ -4,7 +4,28 @@ CREATE EXTENSION pgcrypto;
 -- For a single user
 CREATE TABLE IF NOT EXISTS trader (
     id SERIAL PRIMARY KEY,
-    namr VARCHAR(50),
+    name VARCHAR(50) NOT NULL,
     username VARCHAR(30) UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
+
+-- For a single ticker
+CREATE TABLE IF NOT EXISTS ticker {
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+}
+
+-- For a single stock price
+CREATE TABLE IF NOT EXISTS quote {
+    id SERIAL PRIMARY KEY,
+    ticker_id INT NOT NULL,
+    time TIMESTAMP NOT NULL,
+    price DECIMAL NOT NULL
+}
+
+-- For a single stock trade recommendation
+CREATE TABLE IF NOT EXISTS recommendation {
+    id SERIAL PRIMARY KEY,
+    ticker_id INT NOT NULL,
+    action 
+}
